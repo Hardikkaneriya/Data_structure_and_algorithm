2986c06c6377f1b669c38fc17455561c91ac8014
Hardikkaneriya <45586073+Hardikkaneriya@users.noreply.github.com>
2024-03-27 11:43:32 +0100

    Update Graph.py

--- Graph.py
+++ Graph.py
@@ -15,3 +15,28 @@ def graph(edges):
 edges=[['i','j'],['i','k'],['l','m'],['k','m']]
 graph(edges)
         
+
+
+#DFS with iterative method and recursive method
+graph = { 10 : [20,30,40,50],
+         20:[], 30:[] ,40:[],50:[60],60:[70],70:[]}
+
+
+def dfs_itr(graph,start):
+    stack=[start]
+    while stack:
+        cur= stack.pop()
+        print(cur)
+        for i in graph[cur]:
+            stack.append(i)
+
+def dfs_rec(graph,start):
+    print(start , end=' ')
+    if len(graph[start]==0):
+        return
+    for i in graph[start]:
+        dfs_rec(graph,i)
+    
+
+dfs_rec(graph,10)
+dfs_itr(graph,10)
